853a198d058a4b37ff1a5fc598cc22116d34f61f
Joseph Stocks <jpstocks63@gmail.com>
2023-05-09 11:16:15 -0500

    Add new fonts, section titles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,17 @@
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
+        extend: {
+            // Adds a new breakpoint in addition to the default breakpoints
+            screens: {
+                container: '1100px'
+            }
+        },
         fontFamily: {
-            display: '"Fredericka the Great"'
+            display: ['"Fredericka the Great"', 'serif'],
+            inter: ['Inter', 'sans-serif'],
+            'inter-tight': ['"Inter Tight"', 'sans-serif'],
+            outfit: ['"Outfit"', 'fantasy']
         }
     },
     plugins: []
